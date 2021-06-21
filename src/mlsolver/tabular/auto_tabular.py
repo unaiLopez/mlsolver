1fab7453b7e447ed1faed0d2b0077411319bf068
unailopez <unai19970315@gmail.com>
2021-06-21 21:18:41 +0200

    created auto_tabular classes and sub classes

---
+++ src/mlsolver/tabular/auto_tabular.py
@@ -0,0 +1,25 @@
+class auto_tabular:
+    def __init__(self, X, y, problem_type, algorithms, data_cleaning, feature_engineering, hyperparameter_tuning, ensembles):
+        self.X = X
+        self.y = y
+        self.problem_type = problem_type
+        self.algorithms = algorithms
+        self.data_cleaning = data_cleaning
+        self.feature_engineering = feature_engineering
+        self.hyperparameter_tuning = hyperparameter_tuning
+        self.ensembles = ensembles
+
+    def _do_data_cleaning():
+        pass
+
+    def _do_feature_engineering():
+        pass
+
+    def _do_hyperparameter_tuning():
+        pass
+
+    def _create_ensembles():
+        pass
+    
+    def fit(self, X, y):
+        pass
--- src/mlsolver/tabular/tabular_classifier.py
+++ src/mlsolver/tabular/tabular_classifier.py
@@ -1,13 +1,15 @@
-class tabular_classifier:
-    def __init__(self, X, y, problem_type, algorithms, data_cleaning, feature_engineering, hyperparameter_tuning, ensembles):
-        self.X = X
-        self.y = y
-        self.problem_type = problem_type
-        self.algorithms = algorithms
-        self.data_cleaning = data_cleaning
-        self.feature_engineering = feature_engineering
-        self.hyperparameter_tuning = hyperparameter_tuning
-        self.ensembles = ensembles
+class tabular_classifier(auto_tabular):
+    def __init__(self):
+        super().__init__()
+
+    def _do_data_cleaning():
+        pass
+
+    def _do_feature_engineering():
+        pass
+
+    def _do_hyperparameter_tuning():
+        pass
     
     def fit(self, X, y):
         pass
