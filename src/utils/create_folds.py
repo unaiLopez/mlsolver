15605ac595ad74417d23dfe10b512ef1370e31d7
unailopez <unai19970315@gmail.com>
2021-06-21 21:21:34 +0200

    corrected a typo in create_stratified_kfolds_for_classification method

--- src/utils/create_folds.py
+++ src/utils/create_folds.py
@@ -3,11 +3,20 @@ import pandas as pd
 
 from sklearn.model_selection import KFold, StratifiedKFold
 
-def create_kfolds(data, n_splits=5):
-    kf = KFold(n_splits=n_splits)
+def create_kfolds(data, n_splits=5, shuffle=True):
+    kf = KFold(n_splits=n_splits, shuffle=shuffle)
 
     folds = list()
     for train, test in enumerate(kf.split(data)):
+        folds.append((train, test))
+    
+    return folds
+
+def create_stratified_kfolds_for_classification(data, target_column, n_splits=5, shuffle=True):
+    kf = StratifiedKFold(n_splits, shuffle=shuffle)
+
+    folds = list()
+    for train, test in kf.split(X, data[target_column]):
         folds.append((train, test))
     
     return folds
@@ -18,7 +27,6 @@ def create_stratified_kfolds_for_regression(data, target_column, n_splits=5):
 
     #Apply Sturge's rule
     num_bins = int(np.floor(1 + np.log2(len(data))))
-
     data['bins'] = pd.cut(data[target_column], bins=num_bins, labels=False)
 
     kf = StratifiedKFold(n_splits=n_splits)
